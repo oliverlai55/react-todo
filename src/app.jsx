510f263b52d1fad71f846ad1a924e2663fc71fce
Oliver Lai <oliverlai55@gmail.com>
2016-07-16 15:59:46 -0400

    Update main app layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,31 @@
 var React = require('react');
 var ReactFire = require('reactfire');
+//feeds data from Firebase to component
 var Firebase = require('firebase');
+//get data in Firebase;
+var rootUrl = "https://react-todo-f3296.firebaseio.com/";
 
-var Hello = React.createClass({
+var App = React.createClass({
+  mixins: [ ReactFire ],
+  //a group of methods that sit on one object that gets copied to
+  //another object. so from ReactFire to this component
+  componentWillMount: function() {
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    //creates new instance of Firebase, tell it where to look for data
+  },
   render: function() {
     return (
-    <h1 className="red">
-      Hello!
-    </h1>
+      <div className="row panel panel-default">
+        <div className="col-md-8 col-md-offset-2">
+          <h2 className="text-center">
+            To-Do List
+          </h2>
+        </div>
+
+      </div>
   )
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 React.render(element, document.querySelector('.container'));
