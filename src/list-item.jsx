f6fa5a6281a41390445f1ccbceb4fdd8850a4b53
Oliver Lai <oliverlai55@gmail.com>
2016-07-17 11:31:06 -0400

    Refactor list-item component

---
+++ src/list-item.jsx
@@ -0,0 +1,44 @@
+var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = "https://react-todo-f3296.firebaseio.com/";
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      text: this.props.item.text,
+      done: this.props.item.done
+    }
+  },
+  componentWillMount: function() {
+    this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key);
+  },
+  render: function() {
+    return (
+      <div className="input-group">
+        <span className="input-group-action">
+          <input
+            type="checkbox"
+            checked={this.state.done}
+            onChange={this.handleDoneChange}
+            />
+        </span>
+        <input type="text"
+          className="form-control"
+          value={this.state.text}
+          />
+        <span className="input-group-btn">
+          <button
+            className="btn btn-default"
+            onClick={this.handleDeleteClick}>
+            Delete
+          </button>
+        </span>
+      </div>
+    )
+  },
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked}
+    this.setState(update);
+    this.fb.update(update);
+  }
+})
